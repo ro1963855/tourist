37f3eb530cb82c769b34920bd0b6e70b76f09ff1
Dandy Chen <ro1963855@gmail.com>
2023-11-13 15:26:54 +0800

    setup element-plus env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
+
+// https://element-plus.org/en-US/guide/quickstart.html#on-demand-import
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      dts: './typings/auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [ElementPlusResolver({
+        importStyle: 'sass',
+      })],
+      dts: './typings/components.d.ts',
+    }),
+  ],
 })
