3e9f425f234abe7124e130e1bf6e66fd81380944
Dandy Chen <ro1963855@gmail.com>
2023-11-13 15:52:17 +0800

    setup unocss env

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
+import 'virtual:uno.css';
+// REF: https://unocss.dev/guide/style-reset#tailwind-compat
+import '@unocss/reset/tailwind-compat.css';
+
 import { createApp } from 'vue'
+
 import './style.css'
 import App from './App.vue'
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
+// https://unocss.dev/integrations/vite
+import UnoCSS from 'unocss/vite'
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    UnoCSS(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
       dts: './typings/auto-imports.d.ts',
